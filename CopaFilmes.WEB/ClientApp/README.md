ac945e7c25e12a75ce767ec082dca279d1b2316f
Enderson Cezano <endersoncezano@yahoo.com.br>
2019-07-04 15:08:31 -0300

    arrumando nome do projeto

--- CopaFilmes.WEB/ClientApp/README.md
+++ CopaFilmes.WEB/ClientApp/README.md
@@ -1,4 +1,4 @@
-# CopaFilems.WEB
+# CopaFilmes.WEB
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.0.
 
--- CopaFilmes.WEB/Startup.cs
+++ CopaFilmes.WEB/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace CopaFilems.WEB
+namespace CopaFilmes.WEB
 {
     public class Startup
     {
